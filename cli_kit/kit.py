649bcb5f5c52d8fd52fb017de80fe6e1f5e5a892
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2024-03-14 12:43:32 -0500

    Feat: This is my first commit with my shiny new tool!

---
+++ cli_kit/kit.py
@@ -0,0 +1,41 @@
+import click
+import subprocess
+from pathlib import Path
+from beaupy import confirm, prompt, select, select_multiple
+from rich.console import Console
+
+console = Console()
+
+@click.group()
+def cli():
+	"""
+	This is a command line tool that helps with common git and python tasks.
+	"""
+	pass
+
+@click.command("commit-all")
+def commit_all():
+	"""
+	This command is used to commit all the changes in the current directory.
+	It also asks for a commit message following the Conventional Commits standard.
+	"""
+	commit_scopes = [
+		"Feat",
+		"Refactor",
+		"Fix",
+		"Docs",
+		"Style",
+		"Test",
+		"Chore"
+	]
+
+	console.print("Committing All Changes", style="bold green")
+	commit_type = select(commit_scopes)
+	cmd1 = "git add ."
+	cmd2 = f"git commit -m '{commit_type}: {prompt('Enter the commit message')}'"
+	cmd3 = "git push"
+	subprocess.run(cmd1, shell=True, check=True, cwd=Path.cwd())
+	subprocess.run(cmd2, shell=True, check=True, cwd=Path.cwd())
+	subprocess.run(cmd3, shell=True, check=True, cwd=Path.cwd())
+
+cli.add_command(commit_all)
---
+++ cli_kit/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_namespace_packages
+""" 
+look into curses for the terminal UI
+
+also look into this: https://github.com/charmbracelet/gum
+
+also look into this: https://github.com/petereon/beaupy
+
+"""
+
+setup(
+	name='cli_kit',
+	version='0.1.0',
+	packages=find_namespace_packages(include=['cli_kit', 'cli_kit.*']), # find_
+	install_requires=[
+		'click',
+		'rich',
+		'beaupy'
+	],
+	entry_points='''
+		[console_scripts]
+		kit=kit:cli
+	''',
+)
