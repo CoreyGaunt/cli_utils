c55ae66854875fa24c49f6c849bca738a2fae35a
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2024-04-05 11:58:22 -0500

    Refactor: Updating Save Structure

--- tools/tools.py
+++ tools/tools.py
@@ -170,8 +170,10 @@ def commit():
 		commit_type, commit_message = commit_type_and_message()
 		cmd1 = "git add ."
 		cmd2 = f"git commit -m '{commit_type}: {commit_message}'"
+		save_cmd2 = f'history -s "{cmd2}"'
 		cmd3 = "git push"
 		subprocess.run(cmd1, shell=True, check=True, cwd=Path.cwd())
+		subprocess.run(save_cmd2, shell=True, check=True, cwd=Path.cwd())
 		subprocess.run(cmd2, shell=True, check=True, cwd=Path.cwd())
 		subprocess.run(cmd3, shell=True, check=True, cwd=Path.cwd())
 	else:
@@ -196,11 +198,10 @@ def commit():
 		commit_message, commit_type = commit_type_and_message()
 		cmd2 = f"git commit -m '{commit_type}: {commit_message}'"
 		save_cmd2 = f'history -s "{cmd2}"'
-		print(save_cmd2)
 		cmd3 = "git push"
 		subprocess.run(cmd1, shell=True, check=True, cwd=Path.cwd())
-		subprocess.run(cmd2, shell=True, check=True, cwd=Path.cwd())
 		subprocess.run(save_cmd2, shell=True, check=True, cwd=Path.cwd())
+		subprocess.run(cmd2, shell=True, check=True, cwd=Path.cwd())
 		subprocess.run(cmd3, shell=True, check=True, cwd=Path.cwd())
 
 # TODO: Refactor using Gum commands
