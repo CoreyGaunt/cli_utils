b805f6464d75a61958d595f7bb07c98f0698b791
Corey Gaunt <corey.gaunt@ledgebrook.com>
2024-10-01 17:30:00 -0500

    Feat: update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ https://fig.io/docs/getting-started/first-completion-spec
 
 setup(
 	name='tools',
-	version='0.1.4',
+	version='0.1.5',
 	packages=find_namespace_packages(include=["tools", "tools*"]),
 	package_data={"": ["*.yaml", "*.txt", "*.md"]},
 	author="Corey Gaunt",
