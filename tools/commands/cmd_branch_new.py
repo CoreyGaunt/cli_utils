fa747275515d4b4a7bf992c8cbd61c9d9679e780
Corey Gaunt <corey.gaunt@ledgebrook.com>
2025-02-26 12:41:09 -0600

    Feat: Patch Branch New 2

--- tools/commands/cmd_branch_new.py
+++ tools/commands/cmd_branch_new.py
@@ -36,16 +36,16 @@ def cli():
 	# 	cursor_style=cursor_color
 	# 	)
 	# branch_type = utils.remove_color_indicators(branch_type)
-	has_corresponding_ticket = confirm(
-		question=f"[{prompt_color}]Does this branch have a corresponding ticket?[/{prompt_color}]",
-		cursor=cursor_style,
-		cursor_style=cursor_color,
-		default_is_yes=True
-		)
-	if has_corresponding_ticket:
-		ticket_number = prompt(f"[{prompt_color}]Enter the ticket number[/{prompt_color}]")
-		ticket_number = utils.remove_color_indicators(ticket_number)
-		branch_ticket_ref = f"{config['general']['team-tag']}-{ticket_number}"
+	# has_corresponding_ticket = confirm(
+	# 	question=f"[{prompt_color}]Does this branch have a corresponding ticket?[/{prompt_color}]",
+	# 	cursor=cursor_style,
+	# 	cursor_style=cursor_color,
+	# 	default_is_yes=True
+	# 	)
+	# if has_corresponding_ticket:
+	# 	ticket_number = prompt(f"[{prompt_color}]Enter the ticket number[/{prompt_color}]")
+	# 	ticket_number = utils.remove_color_indicators(ticket_number)
+	# 	branch_ticket_ref = f"{config['general']['team-tag']}-{ticket_number}"
 	branch_name = prompt(f"[{prompt_color}]Enter the branch name[/{prompt_color}]")
 	branch_name = utils.remove_color_indicators(branch_name)
 	pull_default_branch_name = "git rev-parse --abbrev-ref origin/HEAD"
@@ -53,10 +53,10 @@ def cli():
 	default_branch = default_branch_output.stdout.strip()
 	default_branch = default_branch.replace("origin/", "")
 	cmd1 = f"git checkout {default_branch} && git pull"
-	if has_corresponding_ticket:
-		cmd2 = f"git checkout -b {branch_ticket_ref}-{branch_name} && git push --set-upstream origin {branch_ticket_ref}-{branch_name}"
-	else:
-		cmd2 = f"git checkout -b {branch_name} && git push --set-upstream origin {branch_name}"
+	# if has_corresponding_ticket:
+	# 	cmd2 = f"git checkout -b {branch_ticket_ref}-{branch_name} && git push --set-upstream origin {branch_ticket_ref}-{branch_name}"
+	# else:
+	cmd2 = f"git checkout -b {branch_name} && git push --set-upstream origin {branch_name}"
 	spinner = Spinner(LOADING, f"	  Pulling Down From {capwords(default_branch)}")
 	spinner.start()
 	subprocess.run(cmd1, shell=True, check=True, cwd=Path.cwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
